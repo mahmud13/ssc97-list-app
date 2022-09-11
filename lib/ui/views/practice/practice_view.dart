0cf46bce5095748a2416fe58d8a447c996a2e124
shaonkhansur <shaonkhansur@gmail.com>
2022-09-12 02:04:09 +0600

    worc category and diffuculty dorpdown initialized

--- lib/ui/views/practice/practice_view.dart
+++ lib/ui/views/practice/practice_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:mgcs_app/models/words/word.dart';
 import 'package:mgcs_app/ui/layouts/home_layout_view.dart';
 import 'package:mgcs_app/ui/views/practice/answer_widget.dart';
 import 'package:mgcs_app/ui/views/practice/practice_view_model.dart';
@@ -43,6 +44,98 @@ class PracticeView extends StatelessWidget {
                             : Column(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
+                                  model.wordDifficulties.isEmpty &&
+                                          model.wordCategories.isEmpty
+                                      ? const CircularProgressIndicator()
+                                      : Padding(
+                                          padding: const EdgeInsets.all(8),
+                                          child: Row(
+                                            children: [
+                                              Flexible(
+                                                flex: 1,
+                                                child: DropdownButtonFormField(
+                                                  value: model.wordDifficulties[
+                                                      model
+                                                          .wordDifficultyIndex],
+                                                  onChanged:
+                                                      (WordDifficulty? item) {
+                                                    model
+                                                        .onSelectedWordDifficulty(
+                                                            item);
+                                                  },
+                                                  items: model.wordDifficulties
+                                                      .map<
+                                                              DropdownMenuItem<
+                                                                  WordDifficulty>>(
+                                                          (e) {
+                                                    return DropdownMenuItem(
+                                                      value: e,
+                                                      child: Text(e.name),
+                                                    );
+                                                  }).toList(),
+                                                  decoration: InputDecoration(
+                                                      border:
+                                                          OutlineInputBorder(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(10),
+                                                      ),
+                                                      isDense: true,
+                                                      contentPadding:
+                                                          const EdgeInsets
+                                                              .symmetric(
+                                                        horizontal: 10,
+                                                        vertical: 10,
+                                                      ),
+                                                      label: const Text(
+                                                          "Word Difficulty")),
+                                                ),
+                                              ),
+                                              const SizedBox(
+                                                width: 4,
+                                              ),
+                                              Flexible(
+                                                flex: 1,
+                                                child: DropdownButtonFormField(
+                                                  value: model.wordCategories[
+                                                      model.wordCateroryIndex],
+                                                  onChanged:
+                                                      (WordCategory? item) {
+                                                    model
+                                                        .onSelectedWordCategory(
+                                                            item);
+                                                  },
+                                                  items: model.wordCategories
+                                                      .map<
+                                                              DropdownMenuItem<
+                                                                  WordCategory>>(
+                                                          (e) {
+                                                    return DropdownMenuItem(
+                                                      value: e,
+                                                      child: Text(e.name),
+                                                    );
+                                                  }).toList(),
+                                                  decoration: InputDecoration(
+                                                      border:
+                                                          OutlineInputBorder(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(10),
+                                                      ),
+                                                      isDense: true,
+                                                      contentPadding:
+                                                          const EdgeInsets
+                                                              .symmetric(
+                                                        horizontal: 10,
+                                                        vertical: 10,
+                                                      ),
+                                                      label: const Text(
+                                                          "Word Category")),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
                                   Expanded(
                                     child: model.isShowingFeedback
                                         ? const FeedbackWidget()
